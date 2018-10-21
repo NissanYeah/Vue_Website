743b3408c98826122afa7e24182234fb38de2af7
Nissen <handeltonido@gmail.com>
2018-10-22 01:40:49 +0800

    add login

---
+++ src/components/pages/index.vue
@@ -0,0 +1,78 @@
+<template>
+ <div>
+   
+   <h1>index</h1>
+   <a  href="#" @click.prevent="signout">logout</a>
+ 
+</div>
+
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  methods: {
+    signout() {
+      const api = `${process.env.APIPATH}/logout`;
+      var vm = this;
+      this.$http.post(api).then(response => {
+        console.log(response.data.message);
+        if (response.data.success) {
+          vm.$router.push("/login");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
