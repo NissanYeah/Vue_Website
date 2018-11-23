59a4c1aaa21aac7c5e9fb53bcf2d1e9b4c9576f8
Nissen <handeltonido@gmail.com>
2018-11-24 02:11:59 +0800

    取得單一商品

--- src/components/pages/store.vue
+++ src/components/pages/store.vue
@@ -20,26 +20,74 @@
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button type="button" @click.prevent="getProduct(item.id)" class="btn btn-outline-secondary btn-sm">
+              <i v-if="status.loadingItem===item.id" class="fas fa-spinner fa-spin"></i>
               查看更多
             </button>
             <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-spinner fa-spin"></i>
+              <i v-if="status.loadingItem===item.id" class="fas fa-spinner fa-spin"></i>
               加到購物車
             </button>
           </div>
         </div>
       </div>
     </div>
+     <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
+      aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel" >{{product.title}}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.imageUrl" class="img-fluid" alt="">
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{product.content}}</p>
+              <footer class="blockquote-footer text-right"></footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price" >{{product.origin_price}}元</div>
+              <del class="h6" v-if="product.price">原價 {{product.origin_price}}  元</del>
+              <div class="h4" v-if="product.price">現在只要 {{product.price}}  元</div>
+            </div>
+            <select name="" class="form-control mt-3"  v-model="product.num">
+              <option :value="num" v-for="num in 10" :key="num" >
+                  選購 {{num}} {{product.unit}}
+              </option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計 <strong> {{product.num * product.price}} </strong> 元
+            </div>
+            <button type="button" class="btn btn-primary"
+              >
+              <i class="fas fa-spinner fa-spin" ></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  
   </div>
+
 </template>
  <script>
+import $ from "jquery";
 export default {
   data() {
     return {
       products: [],
-      isLoading: false
+      product: {},
+      isLoading: false,
+      status: {
+        loadingItem: ""
+      }
     };
   },
   methods: {
@@ -53,6 +101,21 @@ export default {
         vm.products = response.data.products;
         console.log(response);
         vm.isLoading = false;
+      });
+    },
+    getProduct(id) {
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${
+        process.env.CUSTOMPATH
+      }/product/${id}`;
+      vm.status.loadingItem = id;
+      $("#productModal").modal("show");
+      vm.isLoading = true;
+      this.$http.get(url).then(response => {
+        vm.product = response.data.product;
+        console.log(response);
+        vm.isLoading = false;
+        vm.status.loadingItem = "";
       });
     }
   },
